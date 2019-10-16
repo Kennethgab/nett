7a55318f6c1ac1aaf8e0c5ae36337d896a589adf
Kennethgab <kennega@stud.ntnu.no>
2019-10-16 16:52:39 +0200

    final usecase2

--- usecase2.py
+++ usecase2.py
@@ -23,19 +23,22 @@ def send_email(user, to, body, pwd):
         'Subject:group 4 monitor report {} \n'.format(datetime.datetime.now())
     msg = header + '\n\n'
     msg += body
-    for node in nodes:
-        msg = msg + read_file(node) + '\n'
-
     smtpserver.sendmail(user, to, msg)
     print('done!')
     smtpserver.close()
 
 
 def construct_body(nodes):
+    msg = ''
     for name, values in nodes.items():
-        table = nodes[name][2:]
-        print(tabulate(table, headers=[
-              "Time", "Ipv4 Datagrams", "Difference"]))
+        ip = values[0]
+        msg += "Node:" + name + " TTL: " + values[1]
+        msg += '\n'
+        table = nodes[name][3:]
+        msg += tabulate(table, headers=[
+            "Time", "Ipv4 Datagrams", "Difference"])
+        msg += '\n'
+    return msg
 
 
 def snmpformat(cmd, text):
@@ -65,16 +68,19 @@ def main():
     password = input("Email password for SMTP: ")
     to = input("Email recipient: ")
 
-    # sahara lab pc -> [IP, [time,packets,diff]*]
-    computers = {'sahara17': ['129.241.209.17']}
+    # sahara lab pc -> [IP, TTL,  [time,packets,diff]*]
+    computers = {'sahara17': ['129.241.209.17'],
+                 'sahara18': ['129.241.209.18']}
     com = 'ttm4128'
 
     # ipSystemStatsHCInReceives.ipv4 = .1.3.6.1.2.1.4.31.1.1.19.ipv4 number of ipv4 packets
     datagramsoid = ".1.3.6.1.2.1.4.31.1.1.4.1"
     sysnameoid = " 1.3.6.1.2.1.1.5.0"
+    ttloid = "ipDefaultTTL.0"
 
     # get initial read for pkt diff
-    for name, ip in computers.items():
+    for name, values in computers.items():
+        ip = values[0]
         print("Polling computer {}, with IP {} at time {}".format(
             name, ip, datetime.datetime.now()))
         sysname = os_callout('snmpget', com, ip,
@@ -82,9 +88,13 @@ def main():
         ipv4_datagrams = os_callout(
             "snmpget", com, ip, datagramsoid)
         row = [datetime.datetime.now(), ipv4_datagrams, 0]
+        ttl = os_callout(
+            "snmpget", com, ip, ttloid)
+        computers[name].append(ttl)
         computers[name].append(row)
 
-  #  time.sleep(60)
+    time.sleep(60)
+    tablelength = 0
 # loop every minute, send mail and wipe buffer every 5th minute.
     while True:
         for name, values in computers.items():
@@ -100,11 +110,20 @@ def main():
             row = [datetime.datetime.now(), ipv4_datagrams, int(
                 ipv4_datagrams) - int(prev_pkt_count)]
             computers[name].append(row)
-            table = computers[name][2:]
+            table = computers[name][3:]
+            tablelength = len(table)
             print(tabulate(table, headers=[
-                  "Time", "Ipv4 Datagrams", "Difference"]))
-        if len(table) == 5:
-        construct_body(computers)
+                "Time", "Ipv4 Datagrams", "Difference"]))
+
+        if tablelength == 5:
+            tablelength = 0
+            body = construct_body(computers)
+            print("body of email: ")
+            print(body)
+            send_email(username, to, body, password)
+
+            computers = {'sahara17': ['129.241.209.17'],
+                         'sahara18': ['129.241.209.18']}
 
         time.sleep(60)
 
