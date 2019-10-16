73b6fe710b141745f08c37622666b23e497d0c9d
Kennethgab <kennega@stud.ntnu.no>
2019-10-16 17:03:44 +0200

    usecase1

---
+++ usecase1.py
@@ -0,0 +1,43 @@
+import subprocess
+import re
+import datetime
+from tabulate import tabulate
+import time
+
+
+def snmpformat(cmd, text):
+    # filters to only data requested based on cmd used
+
+    regs = {
+        'snmpget': '= \w+: (.+)'
+    }
+
+    m = re.search(regs[cmd], text.decode("utf-8"))
+    if m:
+        return m.group(1)
+
+
+def os_callout(cmd, com, ip, oid):
+
+    snmpcmd = "{} -v 2c -c {} {} {}".format(
+        cmd, com, ip, oid)
+    p = subprocess.Popen(snmpcmd, stdout=subprocess.PIPE, shell=True)
+    output = p.communicate()[0]
+    output = snmpformat(cmd, output)
+    return output
+
+
+def main():
+
+    datagramsoid = ".1.3.6.1.2.1.4.31.1.1.4.1"
+    sysnameoid = " 1.3.6.1.2.1.1.5.0"
+    ttloid = "ipDefaultTTL.0"
+
+    ip = "129.241.209.17"
+    com = "ttm4128"
+    datagrams = os_callout("snmpget", com, ip, datagramsoid)
+    print(datagrams)
+
+
+if __name__ == "__main__":
+    main()
